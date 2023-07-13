82619918a29a47f08c07e9e180d1694db95c805d
manohar <75294773+gowreemanohar@users.noreply.github.com>
2023-07-14 03:23:51 +0530

    props were initiated and managed the redundant code.

--- src/App.js
+++ src/App.js
@@ -18,13 +18,22 @@ function App() {
 
       <div className="benefitsSection mt-12 lg:flex bg-white">
         <div className="benefits lg:w-1/2 ">
-          <BenefitCard
+          <BenefitCard 
+          title="Benefit 1"
+          text="Highlight the benefits of signing up for an appointment, online class, or video consultation.online class, or video consultation"
+          imagelink="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/13b0f11a-multor-benefit1-icon.svg"
           >
           </BenefitCard>
-          <BenefitCard
+          <BenefitCard 
+          title="Benefit 2"
+          text="Highlight the benefits of signing up for an appointment, online class, or video consultation.online class, or video consultation"
+          imagelink="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/8ab92ea8-multor-benefit2-icon.svg"
           >  
           </BenefitCard>
           <BenefitCard
+          title="Benefit 3"
+          text="Highlight the benefits of signing up for an appointment, online class, or video consultation.online class, or video consultation"
+           imagelink="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/c32dd90d-multor-benefit3-icon.svg"
           >
           </BenefitCard>
         </div>
@@ -37,16 +46,37 @@ function App() {
 
     <div className="reviews lg:flex">
       <BenefitCard
+      title="anna"
+      text="Highlight the benefits of signing up for an appointment, online class, or video consultation.online class, or video consultation"
+      imagelink="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/52bdc545-multor-testimonial1-headshot_100000000000000000001o.jpg"
       >
       </BenefitCard>
       <BenefitCard
+      title="martha"
+      text="Highlight the benefits of signing up for an appointment, online class, or video consultation.online class, or video consultation"
+      imagelink="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/94045dbc-multor-testimonial2-headshot_100000000000000000001o.jpg"
       >
       </BenefitCard>
     </div>
     <div className="features lg:mt-12 mt-8 lg:px-16 lg:flex">
-      <FeaturesCard></FeaturesCard>
-      <FeaturesCard></FeaturesCard>
-      <FeaturesCard></FeaturesCard>
+      <FeaturesCard
+      title="feature 1"
+      imagelink="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/53ad3716-multor-feature1-image_100000000000000000001o.jpg"
+      text="Talk about some of the details of your offer with a focus on the value people get back.focus on the value people get back."
+      
+      ></FeaturesCard>
+      <FeaturesCard
+      title="feature 2"
+      imagelink="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/adf66a57-multor-feature2-image_100000000000000000001o.jpg"
+      text="Talk about some of the details of your offer with a focus on the value people get back.focus on the value people get back.">
+
+      </FeaturesCard>
+      <FeaturesCard
+      title="feature 3"
+      imagelink="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/6d486c61-multor-feature3-image_100000000000000000001o.jpg"
+      text="Talk about some of the details of your offer with a focus on the value people get back.focus on the value people get back.">
+
+      </FeaturesCard>
 
     </div>
 
--- src/Components/FeaturesCard.jsx
+++ src/Components/FeaturesCard.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const FeaturesCard = () => {
+const FeaturesCard = (props) => {
   return (
     <div>
         <div className="   drop-shadow-lg  bg-white rounded-3xl pb-4 lg:mx-4 lg:my-0 my-4">
-            <img src="https://d9hhrg4mnvzow.cloudfront.net/templates.unbounce.com/multor/53ad3716-multor-feature1-image_100000000000000000001o.jpg" alt='jharsuguda airport' 
+            <img src={props.imagelink} alt='features' 
             className='w-full lg:h-64 rounded-3xl -4 '>
             </img>
-            <h3 className='mt-4 text-3xl text-center  font-semibold leading-none text-footerblue dark:text-white '>Feature 1 </h3>
+            <h3 className='mt-4 text-3xl text-center  font-semibold leading-none text-footerblue dark:text-white '>{props.title} </h3>
             <p className='text-md  mt-4 lg:px-12'> 
-            Talk about some of the details of your offer with a focus on the value people get back.focus on the value people get back.</p>                    
+           {props.text}</p>                    
         </div>
     </div>
   )
